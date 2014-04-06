e56bbd5a442962719f55592588499d29c774acdf
cameron581 <cameron581@cox.net>
2014-04-05 20:30:31 -0500

    initial jenkins for drdevs modify from here

---
+++ build.sh
@@ -0,0 +1,317 @@
+#!/usr/bin/env bash
+
+function check_result {
+  if [ "0" -ne "$?" ]
+  then
+    (repo forall -c "git reset --hard") >/dev/null
+    rm -f .repo/local_manifests/dyn-*.xml
+    rm -f .repo/local_manifests/roomservice.xml
+    echo $1
+    exit 1
+  fi
+}
+
+if [ -z "$HOME" ]
+then
+  echo HOME not in environment, guessing...
+  export HOME=$(awk -F: -v v="$USER" '{if ($1==v) print $6}' /etc/passwd)
+fi
+
+if [ -z "$WORKSPACE" ]
+then
+  echo WORKSPACE not specified
+  exit 1
+fi
+
+if [ -z "$CLEAN" ]
+then
+  echo CLEAN not specified
+  exit 1
+fi
+
+if [ -z "$REPO_BRANCH" ]
+then
+  echo REPO_BRANCH not specified
+  exit 1
+fi
+
+if [ -z "$LUNCH" ]
+then
+  echo LUNCH not specified
+  exit 1
+fi
+
+if [ -z "$RELEASE_TYPE" ]
+then
+  echo RELEASE_TYPE not specified
+  exit 1
+fi
+
+if [ -z "$SYNC_PROTO" ]
+then
+  SYNC_PROTO=http
+fi
+
+# colorization fix in Jenkins
+export CL_RED="\"\033[31m\""
+export CL_GRN="\"\033[32m\""
+export CL_YLW="\"\033[33m\""
+export CL_BLU="\"\033[34m\""
+export CL_MAG="\"\033[35m\""
+export CL_CYN="\"\033[36m\""
+export CL_RST="\"\033[0m\""
+
+cd $WORKSPACE
+rm -rf archive
+mkdir -p archive
+export BUILD_NO=$BUILD_NUMBER
+unset BUILD_NUMBER
+
+export PATH=~/bin:$PATH
+
+export USE_CCACHE=1
+export CCACHE_NLEVELS=4
+export BUILD_WITH_COLORS=0
+
+REPO=$(which repo)
+if [ -z "$REPO" ]
+then
+  mkdir -p ~/bin
+  curl http://commondatastorage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
+  chmod a+x ~/bin/repo
+fi
+
+git config --global user.name $(whoami)@$NODE_NAME
+git config --global user.email rickle581@gmail.com
+
+mkdir -p $REPO_BRANCH
+cd $REPO_BRANCH
+
+# always force a fresh repo init since we can build off different branches
+# and the "default" upstream branch can get stuck on whatever was init first.
+if [ -z "$CORE_BRANCH" ]
+then
+  CORE_BRANCH=$REPO_BRANCH
+fi
+
+if [ ! -z "$RELEASE_MANIFEST" ]
+then
+  MANIFEST="-m $RELEASE_MANIFEST"
+else
+  RELEASE_MANIFEST=""
+  MANIFEST=""
+fi
+
+rm -rf .repo/manifests*
+rm -f .repo/local_manifests/dyn-*.xml
+repo init -u $SYNC_PROTO://github.com/soupy031/LiquidSmooth.git -b $CORE_BRANCH $MANIFEST
+check_result "repo init failed."
+
+# make sure ccache is in PATH
+if [[ "$REPO_BRANCH" =~ "jellybean" || $REPO_BRANCH =~ "cm-10" ]]
+then
+export PATH="$PATH:/opt/local/bin/:$PWD/prebuilts/misc/$(uname|awk '{print tolower($0)}')-x86/ccache"
+export CCACHE_DIR=~/.jb_ccache
+else
+export PATH="$PATH:/opt/local/bin/:$PWD/prebuilt/$(uname|awk '{print tolower($0)}')-x86/ccache"
+export CCACHE_DIR=~/.ics_ccache
+fi
+
+if [ -f ~/.jenkins_profile ]
+then
+  . ~/.jenkins_profile
+fi
+
+mkdir -p .repo/local_manifests
+rm -f .repo/local_manifest.xml
+
+if [ ! -z "$LOCAL_MANIFEST" ]
+then
+  echo $LOCAL_MANIFEST > .repo/local_manifests/$LUNCH.xml
+fi
+
+echo Core Manifest:
+cat .repo/manifest.xml
+
+## TEMPORARY: Some kernels are building _into_ the source tree and messing
+## up posterior syncs due to changes
+rm -rf kernel/*
+
+echo Syncing...
+repo sync -d -c > /dev/null
+check_result "repo sync failed."
+echo Sync complete.
+
+if [ -f $WORKSPACE/jenkins/$REPO_BRANCH-setup.sh ]
+then
+  $WORKSPACE/jenkins/$REPO_BRANCH-setup.sh
+else
+  $WORKSPACE/jenkins/cm-setup.sh
+fi
+
+if [ -f .last_branch ]
+then
+  LAST_BRANCH=$(cat .last_branch)
+else
+  echo "Last build branch is unknown, assume clean build"
+  LAST_BRANCH=$REPO_BRANCH-$CORE_BRANCH$RELEASE_MANIFEST
+fi
+
+if [ "$LAST_BRANCH" != "$REPO_BRANCH-$CORE_BRANCH$RELEASE_MANIFEST" ]
+then
+  echo "Branch has changed since the last build happened here. Forcing cleanup."
+  CLEAN="true"
+fi
+
+. build/envsetup.sh
+# Workaround for failing translation checks in common hardware repositories
+if [ ! -z "$GERRIT_XLATION_LINT" ]
+then
+    LUNCH=$(echo $LUNCH@$DEVICEVENDOR | sed -f $WORKSPACE/jenkins/shared-repo.map)
+fi
+
+# lunch for the first time, to grab roomservice stuff if any
+lunch $LUNCH
+check_result "lunch failed."
+
+# save manifest used for build (saving revisions as current HEAD)
+repo manifest -o $WORKSPACE/archive/manifest.xml -r
+
+# clear patches so change logs don't show them every time
+echo Syncing to clear patches...
+repo sync -d -c > /dev/null
+echo Sync complete.
+
+rm -f $OUT/cm-*.zip*
+
+UNAME=$(uname)
+
+if [ ! -z "$BUILD_USER_ID" ]
+then
+  export RELEASE_TYPE=CM_EXPERIMENTAL
+fi
+
+if [ "$RELEASE_TYPE" = "CM_NIGHTLY" ]
+then
+  if [ "$REPO_BRANCH" = "gingerbread" ]
+  then
+    export CYANOGEN_NIGHTLY=true
+  else
+    export CM_NIGHTLY=true
+  fi
+elif [ "$RELEASE_TYPE" = "CM_EXPERIMENTAL" ]
+then
+  export CM_EXPERIMENTAL=true
+elif [ "$RELEASE_TYPE" = "CM_RELEASE" ]
+then
+  # gingerbread needs this
+  export CYANOGEN_RELEASE=true
+  # ics needs this
+  export CM_RELEASE=true
+fi
+
+if [ ! -z "$CM_EXTRAVERSION" ]
+then
+  export CM_EXPERIMENTAL=true
+fi
+
+if [ ! -z "$GERRIT_CHANGES" ]
+then
+  export CM_EXPERIMENTAL=true
+  IS_HTTP=$(echo $GERRIT_CHANGES | grep http)
+  if [ -z "$IS_HTTP" ]
+  then
+    python $WORKSPACE/jenkins/repopick.py $GERRIT_CHANGES
+    check_result "gerrit picks failed."
+  else
+    python $WORKSPACE/jenkins/repopick.py $(curl $GERRIT_CHANGES)
+    check_result "gerrit picks failed."
+  fi
+  if [ ! -z "$GERRIT_XLATION_LINT" ]
+  then
+    python $WORKSPACE/jenkins/xlationlint.py $GERRIT_CHANGES
+    check_result "basic XML lint failed."
+  fi
+fi
+
+if [ ! "$(ccache -s|grep -E 'max cache size'|awk '{print $4}')" = "100.0" ]
+then
+  ccache -M 100G
+fi
+
+rm -f $WORKSPACE/changecount
+WORKSPACE=$WORKSPACE LUNCH=$LUNCH bash $WORKSPACE/jenkins/changes/buildlog.sh 2>&1
+if [ -f $WORKSPACE/changecount ]
+then
+#  CHANGE_COUNT=$(cat $WORKSPACE/changecount)
+  rm -f $WORKSPACE/changecount
+#  if [ $CHANGE_COUNT -eq "0" ]
+#  then
+#    echo "Zero changes since last build, aborting"
+#    exit 0
+#  fi
+fi
+
+# lunch again to add any patches now that the change log has been generated
+. build/envsetup.sh
+lunch $LUNCH
+check_result "lunch failed."
+
+LAST_CLEAN=0
+if [ -f .clean ]
+then
+  LAST_CLEAN=$(date -r .clean +%s)
+fi
+TIME_SINCE_LAST_CLEAN=$(expr $(date +%s) - $LAST_CLEAN)
+# convert this to hours
+TIME_SINCE_LAST_CLEAN=$(expr $TIME_SINCE_LAST_CLEAN / 60 / 60)
+if [ $TIME_SINCE_LAST_CLEAN -gt "21" -o $CLEAN = "true" ]
+then
+  echo "Cleaning!"
+  touch .clean
+  make clobber
+else
+  echo "Skipping clean: $TIME_SINCE_LAST_CLEAN hours since last clean."
+fi
+
+echo "$REPO_BRANCH-$CORE_BRANCH$RELEASE_MANIFEST" > .last_branch
+
+time mka liquid recoveryzip recoveryimage checkapi
+check_result "Build failed."
+
+for f in $(ls $OUT/cm-*.zip*)
+do
+  ln $f $WORKSPACE/archive/$(basename $f)
+done
+if [ -f $OUT/utilties/update.zip ]
+then
+  cp $OUT/utilties/update.zip $WORKSPACE/archive/recovery.zip
+fi
+if [ -f $OUT/recovery.img ]
+then
+  cp $OUT/recovery.img $WORKSPACE/archive
+fi
+
+# archive the build.prop as well
+ZIP=$(ls $WORKSPACE/archive/cm-*.zip)
+unzip -p $ZIP system/build.prop > $WORKSPACE/archive/build.prop
+
+# CORE: save manifest used for build (saving revisions as current HEAD)
+rm -f .repo/local_manifests/dyn-$REPO_BRANCH.xml
+rm -f .repo/local_manifests/roomservice.xml
+
+# Stash away other possible manifests
+TEMPSTASH=$(mktemp -d)
+mv .repo/local_manifests $TEMPSTASH
+
+repo manifest -o $WORKSPACE/archive/core.xml -r
+
+mv $TEMPSTASH/local_manifests .repo
+rmdir $TEMPSTASH
+
+# chmod the files in case UMASK blocks permissions
+chmod -R ugo+r $WORKSPACE/archive
+
+echo Syncing to clear patches...
+repo sync -d -c > /dev/null
+echo Sync complete.
---
+++ repopick.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+
+import sys
+import json
+import os
+import subprocess
+import re
+
+try:
+  # For python3
+  import urllib.request
+except ImportError:
+  # For python2
+  import imp
+  import urllib2
+  urllib = imp.new_module('urllib')
+  urllib.request = urllib2
+
+for change in sys.argv[1:]:
+    print(change)
+    f = urllib.request.urlopen('http://review.cyanogenmod.com/query?q=change:%s' % change)
+    d = f.read().decode()
+    # gerrit doesnt actually return json. returns two json blobs, separate lines. bizarre.
+    print(d)
+    d = d.split('\n')[0]
+    data = json.loads(d)
+    project = data['project']
+
+    plist = subprocess.Popen(["/usr/bin/repo","list"], stdout=subprocess.PIPE)
+    while(True):
+        retcode = plist.poll()
+        pline = plist.stdout.readline().rstrip()
+        ppaths = re.split('\s*:\s*', pline.decode())
+        if ppaths[1] == project:
+            project = ppaths[0]
+            break
+        if(retcode is not None):
+            break
+
+    print(project)
+    number = data['number']
+    patch_count = 0
+    junk = number[len(number) - 2:]
+
+    if not os.path.isdir(project):
+        sys.stderr.write('no project directory: %s' % project)
+        sys.exit(1)
+
+    while 0 != os.system('cd %s ; git fetch http://review.cyanogenmod.com/%s refs/changes/%s/%s/%s' % (project, data['project'], junk, number, patch_count + 1)):
+        patch_count = patch_count + 1
+
+    while 0 == os.system('cd %s ; git fetch http://review.cyanogenmod.com/%s refs/changes/%s/%s/%s' % (project, data['project'], junk, number, patch_count + 1)):
+        patch_count = patch_count + 1
+
+    os.system('cd %s ; git fetch http://review.cyanogenmod.com/%s refs/changes/%s/%s/%s' % (project, data['project'], junk, number, patch_count))
+    os.system('cd %s ; git merge FETCH_HEAD' % project)
